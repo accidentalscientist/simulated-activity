5b31545cc377ea9387dfba5909da50e8cd7ed129
accidentalscientist <t.aymonier.newman@gmail.com>
2020-01-10 18:03:50 +1100

    solve rolling median window in python

---
+++ archive/python/2020/20200110_02_rolling_median_window.py
@@ -0,0 +1,18 @@
+"""Archive task: Rolling Median Window.
+
+Generated for 2020-01-10. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+from statistics import mean
+
+
+def rolling_average(values, window):
+    if window <= 0:
+        raise ValueError("window must be positive")
+    return [mean(values[index:index + window]) for index in range(len(values) - window + 1)]
+
+
+if __name__ == "__main__":
+    sample = [3, 5, 8, 13, 21, 34]
+    print(rolling_average(sample, 3))
