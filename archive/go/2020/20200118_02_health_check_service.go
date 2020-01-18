645016dbec867013a066bfb27f86249faa278ec8
accidentalscientist <t.aymonier.newman@gmail.com>
2020-01-18 12:11:30 +1100

    add health check service

---
+++ archive/go/2020/20200118_02_health_check_service.go
@@ -0,0 +1,12 @@
+// Archive task: Health Check Service
+// Generated for 2020-01-18
+
+package archive
+
+func SumByKey(rows []map[string]int, key string) int {
+	total := 0
+	for _, row := range rows {
+		total += row[key]
+	}
+	return total
+}
