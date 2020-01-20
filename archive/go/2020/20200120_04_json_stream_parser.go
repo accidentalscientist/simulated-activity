e49597973350c1ea4cc0e5498c1e869c80d9e7cc
accidentalscientist <t.aymonier.newman@gmail.com>
2020-01-20 17:35:42 +1100

    add json stream parser

---
+++ archive/go/2020/20200120_04_json_stream_parser.go
@@ -0,0 +1,12 @@
+// Archive task: Json Stream Parser
+// Generated for 2020-01-20
+
+package archive
+
+func SumByKey(rows []map[string]int, key string) int {
+	total := 0
+	for _, row := range rows {
+		total += row[key]
+	}
+	return total
+}
