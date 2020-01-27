ef75a2bd0bb98fc3043f35c4377ef121c2fd8ac3
accidentalscientist <t.aymonier.newman@gmail.com>
2020-01-27 19:05:10 +1100

    implement bounded queue in rust

---
+++ archive/rust/2020/20200127_03_bounded_queue.rs
@@ -0,0 +1,20 @@
+// Archive task: Bounded Queue
+// Generated for 2020-01-27
+
+pub fn bounded_sum(values: &[i64], limit: i64) -> i64 {
+    values
+        .iter()
+        .copied()
+        .filter(|value| *value <= limit)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sums_values_under_limit() {
+        assert_eq!(bounded_sum(&[2, 5, 8, 13], 8), 15);
+    }
+}
