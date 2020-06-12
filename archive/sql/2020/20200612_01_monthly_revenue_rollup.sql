d73f475c680e44e2bd42b49ca01d3c6f3497e4c5
accidentalscientist <t.aymonier.newman@gmail.com>
2020-06-12 10:58:05 +1000

    add monthly revenue rollup query

---
+++ archive/sql/2020/20200612_01_monthly_revenue_rollup.sql
@@ -0,0 +1,10 @@
+-- Archive task: Monthly Revenue Rollup
+-- Generated for 2020-06-12
+
+SELECT
+    DATE_TRUNC('month', invoice_date) AS revenue_month,
+    SUM(amount) AS gross_revenue,
+    COUNT(DISTINCT customer_id) AS paying_customers
+FROM invoices
+GROUP BY DATE_TRUNC('month', invoice_date)
+ORDER BY revenue_month;
