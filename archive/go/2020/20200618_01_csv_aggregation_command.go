033504a25fbe4656615d644cc049c58df8a92063
accidentalscientist <t.aymonier.newman@gmail.com>
2020-06-18 11:30:42 +1000

    aggregate csv metrics in go

---
+++ archive/go/2020/20200618_01_csv_aggregation_command.go
@@ -0,0 +1,12 @@
+// Archive task: Csv Aggregation Command
+// Generated for 2020-06-18
+
+package archive
+
+func SumByKey(rows []map[string]int, key string) int {
+	total := 0
+	for _, row := range rows {
+		total += row[key]
+	}
+	return total
+}
