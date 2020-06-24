2468f86f11f2e17c1c0f3cec64fc7b765c93cab8
accidentalscientist <t.aymonier.newman@gmail.com>
2020-06-24 17:23:26 +1000

    add request validator in typescript

---
+++ archive/typescript/2020/20200624_04_request_validator.ts
@@ -0,0 +1,11 @@
+// Archive task: Request Validator
+// Generated for 2020-06-24
+
+type RequestBody = { email?: string; amount?: number };
+
+export function validateRequest(body: RequestBody): string[] {
+  const errors: string[] = [];
+  if (!body.email?.includes("@")) errors.push("email is invalid");
+  if (body.amount === undefined || body.amount <= 0) errors.push("amount must be positive");
+  return errors;
+}
