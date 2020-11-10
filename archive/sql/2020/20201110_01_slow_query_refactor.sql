90c6a6212f5351a7716ab787aedda51595415458
accidentalscientist <t.aymonier.newman@gmail.com>
2020-11-10 11:11:35 +1100

    refactor reporting query with ctes

---
+++ archive/sql/2020/20201110_01_slow_query_refactor.sql
@@ -0,0 +1,17 @@
+-- Archive task: Slow Query Refactor
+-- Generated for 2020-11-10
+
+WITH ranked_orders AS (
+    SELECT
+        customer_id,
+        order_id,
+        order_total,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id
+            ORDER BY order_total DESC
+        ) AS order_rank
+    FROM orders
+)
+SELECT customer_id, order_id, order_total
+FROM ranked_orders
+WHERE order_rank <= 3;
