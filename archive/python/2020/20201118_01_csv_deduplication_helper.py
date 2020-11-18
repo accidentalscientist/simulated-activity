1a8f83279a6a15f8884da12accd42b8e237c2ca0
accidentalscientist <t.aymonier.newman@gmail.com>
2020-11-18 11:16:06 +1100

    add csv deduplication helper

---
+++ archive/python/2020/20201118_01_csv_deduplication_helper.py
@@ -0,0 +1,19 @@
+"""Archive task: Csv Deduplication Helper.
+
+Generated for 2020-11-18. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+from collections.abc import Iterable
+
+
+def deduplicate_records(records: Iterable[dict], keys: tuple[str, ...]) -> list[dict]:
+    seen = set()
+    unique = []
+    for record in records:
+        signature = tuple(record.get(key) for key in keys)
+        if signature in seen:
+            continue
+        seen.add(signature)
+        unique.append(record)
+    return unique
