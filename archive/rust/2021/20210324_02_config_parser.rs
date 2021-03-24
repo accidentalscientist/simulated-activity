4b6b13beebd4105981e52cdbd07f08f9a360a466
accidentalscientist <t.aymonier.newman@gmail.com>
2021-03-24 14:05:58 +1100

    parse config blocks in rust

---
+++ archive/rust/2021/20210324_02_config_parser.rs
@@ -0,0 +1,10 @@
+// Archive task: Config Parser
+// Generated for 2021-03-24
+
+pub fn parse_pairs(input: &str) -> Vec<(&str, &str)> {
+    input
+        .lines()
+        .filter_map(|line| line.split_once('='))
+        .map(|(key, value)| (key.trim(), value.trim()))
+        .collect()
+}
