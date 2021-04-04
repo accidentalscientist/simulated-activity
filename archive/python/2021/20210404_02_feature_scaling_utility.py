053f8898bd6db17b16cfed5d388dc517ed05a610
accidentalscientist <t.aymonier.newman@gmail.com>
2021-04-04 17:23:18 +1000

    add feature scaling utility

---
+++ archive/python/2021/20210404_02_feature_scaling_utility.py
@@ -0,0 +1,13 @@
+"""Archive task: Feature Scaling Utility.
+
+Generated for 2021-04-04. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+def min_max_scale(values: list[float]) -> list[float]:
+    if not values:
+        return []
+    low, high = min(values), max(values)
+    if high == low:
+        return [0.0 for _ in values]
+    return [(value - low) / (high - low) for value in values]
