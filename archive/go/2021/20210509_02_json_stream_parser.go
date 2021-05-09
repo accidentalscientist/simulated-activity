c38398a28b176078b29f0d1bf7a2df63a11007c2
accidentalscientist <t.aymonier.newman@gmail.com>
2021-05-09 15:12:22 +1000

    add json stream parser

---
+++ archive/go/2021/20210509_02_json_stream_parser.go
@@ -0,0 +1,10 @@
+// Archive task: Json Stream Parser
+// Generated for 2021-05-09
+
+package archive
+
+import "encoding/json"
+
+func DecodeObjects(data []byte, target any) error {
+	return json.Unmarshal(data, target)
+}
