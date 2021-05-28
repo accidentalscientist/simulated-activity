1e9734a8a8972c0664437a744624a7993998fe49
accidentalscientist <t.aymonier.newman@gmail.com>
2021-05-28 15:20:01 +1000

    add health check service

---
+++ archive/go/2021/20210528_02_health_check_service.go
@@ -0,0 +1,18 @@
+// Archive task: Health Check Service
+// Generated for 2021-05-28
+
+package archive
+
+type HealthCheck struct {
+	Name string
+	OK   bool
+}
+
+func OverallStatus(checks []HealthCheck) bool {
+	for _, check := range checks {
+		if !check.OK {
+			return false
+		}
+	}
+	return true
+}
