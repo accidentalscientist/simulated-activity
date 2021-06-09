d719f38829382492a35e2a7d1a54d4df15b5df14
accidentalscientist <t.aymonier.newman@gmail.com>
2021-06-09 11:19:34 +1000

    add date range guard

---
+++ archive/typescript/2021/20210609_01_date_range_guard.ts
@@ -0,0 +1,6 @@
+// Archive task: Date Range Guard
+// Generated for 2021-06-09
+
+export function isWithinRange(value: Date, start: Date, end: Date): boolean {
+  return value.getTime() >= start.getTime() && value.getTime() <= end.getTime();
+}
