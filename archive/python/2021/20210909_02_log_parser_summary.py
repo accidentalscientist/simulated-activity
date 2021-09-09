acaf76d755cfa85f922930183aec3e27762caba4
accidentalscientist <t.aymonier.newman@gmail.com>
2021-09-09 18:01:59 +1000

    summarise service logs with python

---
+++ archive/python/2021/20210909_02_log_parser_summary.py
@@ -0,0 +1,16 @@
+"""Archive task: Log Parser Summary.
+
+Generated for 2021-09-09. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+from collections import Counter
+
+
+def summarise_status_codes(lines: list[str]) -> dict[str, int]:
+    counts = Counter()
+    for line in lines:
+        parts = line.split()
+        if len(parts) >= 2:
+            counts[parts[-1]] += 1
+    return dict(counts)
