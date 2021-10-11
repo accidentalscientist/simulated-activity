cc3bf104ebb1cc16be6b143f05653ca2d0d6fb97
accidentalscientist <t.aymonier.newman@gmail.com>
2021-10-11 15:30:40 +1100

    cover python edge cases

---
+++ archive/python/2021/20211011_03_unit_tests_for_edge_cases.py
@@ -0,0 +1,23 @@
+"""Archive task: Unit Tests For Edge Cases.
+
+Generated for 2021-10-11. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+import unittest
+
+
+def clamp(value: float, low: float, high: float) -> float:
+    return max(low, min(high, value))
+
+
+class EdgeCaseTests(unittest.TestCase):
+    def test_clamps_below_range(self):
+        self.assertEqual(clamp(-4, 0, 10), 0)
+
+    def test_clamps_above_range(self):
+        self.assertEqual(clamp(18, 0, 10), 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
