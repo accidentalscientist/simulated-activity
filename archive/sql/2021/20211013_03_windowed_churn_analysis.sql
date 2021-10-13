567f033730a0b44b14cadece3853e97bc5e14b44
accidentalscientist <t.aymonier.newman@gmail.com>
2021-10-13 15:50:00 +1100

    model churn with sql windows

---
+++ archive/sql/2021/20211013_03_windowed_churn_analysis.sql
@@ -0,0 +1,13 @@
+-- Archive task: Windowed Churn Analysis
+-- Generated for 2021-10-13
+
+SELECT
+    customer_id,
+    activity_month,
+    active_days,
+    LAG(active_days) OVER (
+        PARTITION BY customer_id
+        ORDER BY activity_month
+    ) AS previous_active_days
+FROM monthly_customer_activity
+WHERE active_days = 0;
