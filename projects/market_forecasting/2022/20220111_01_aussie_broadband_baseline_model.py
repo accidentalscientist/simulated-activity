31b2dc8c312bc1e14d7691b547684e381af91c9e
accidentalscientist <t.aymonier.newman@gmail.com>
2022-01-11 12:14:03 +1100

    fit aussie broadband baseline model

---
+++ projects/market_forecasting/2022/20220111_01_aussie_broadband_baseline_model.py
@@ -0,0 +1,9 @@
+"""Archive task: Aussie Broadband Baseline Model.
+
+Generated for 2022-01-11. This synthetic market research code is educational
+only and is not financial advice.
+"""
+
+def fit_baseline_forecast(history: list[float], horizon: int = 5) -> list[float]:
+    slope = (history[-1] - history[0]) / max(len(history) - 1, 1)
+    return [history[-1] + slope * step for step in range(1, horizon + 1)]
