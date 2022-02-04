29c85ee37b054062a753dca3652924b1653cd126
accidentalscientist <t.aymonier.newman@gmail.com>
2022-02-04 20:18:18 +1100

    solve graph traversal in rust

---
+++ archive/rust/2022/20220204_06_graph_traversal.rs
@@ -0,0 +1,17 @@
+// Archive task: Graph Traversal
+// Generated for 2022-02-04
+
+use std::collections::{HashMap, HashSet, VecDeque};
+
+pub fn reachable(graph: &HashMap<&str, Vec<&str>>, start: &str) -> HashSet<String> {
+    let mut seen = HashSet::new();
+    let mut queue = VecDeque::from([start]);
+    while let Some(node) = queue.pop_front() {
+        if seen.insert(node.to_string()) {
+            if let Some(neighbours) = graph.get(node) {
+                queue.extend(neighbours.iter().copied());
+            }
+        }
+    }
+    seen
+}
