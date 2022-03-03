0c9d5880229422162b116ea98bfddf64174e25b8
accidentalscientist <t.aymonier.newman@gmail.com>
2022-03-03 14:07:30 +1100

    add evera lithium factor set

---
+++ projects/market_forecasting/2022/20220303_02_evera_lithium_factor_set.py
@@ -0,0 +1,12 @@
+"""Archive task: Evera Lithium Factor Set.
+
+Generated for 2022-03-03. This synthetic market research code is educational
+only and is not financial advice.
+"""
+
+def build_lithium_factor_set(prices, volumes, benchmark):
+    return {
+        "momentum_20": prices[-1] / prices[-20] - 1,
+        "volume_ratio": volumes[-5] / max(sum(volumes[-20:]) / 20, 1),
+        "benchmark_spread": prices[-1] / benchmark[-1] - 1,
+    }
