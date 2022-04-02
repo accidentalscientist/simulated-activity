64c15d12b7fd763421dfb2bdeff941846c676cfa
accidentalscientist <t.aymonier.newman@gmail.com>
2022-04-02 17:58:51 +1100

    add json stream parser

---
+++ archive/go/2022/20220402_02_json_stream_parser.go
@@ -0,0 +1,10 @@
+// Archive task: Json Stream Parser
+// Generated for 2022-04-02
+
+package archive
+
+import "encoding/json"
+
+func DecodeObjects(data []byte, target any) error {
+	return json.Unmarshal(data, target)
+}
