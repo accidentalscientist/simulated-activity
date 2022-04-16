c99f9d109f094aabc986e0116c028fe305e06396
accidentalscientist <t.aymonier.newman@gmail.com>
2022-04-16 20:02:41 +1000

    implement worker pool in go

---
+++ archive/go/2022/20220416_05_worker_pool.go
@@ -0,0 +1,12 @@
+// Archive task: Worker Pool
+// Generated for 2022-04-16
+
+package archive
+
+func RunJobs(jobs []func() int) []int {
+	results := make([]int, 0, len(jobs))
+	for _, job := range jobs {
+		results = append(results, job())
+	}
+	return results
+}
