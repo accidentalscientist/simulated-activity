7956508bff1a9882eea8353df479152a85706cfb
accidentalscientist <t.aymonier.newman@gmail.com>
2022-04-16 15:50:03 +1000

    normalise event payloads

---
+++ archive/typescript/2022/20220416_03_normalise_payload_helper.ts
@@ -0,0 +1,16 @@
+// Archive task: Normalise Payload Helper
+// Generated for 2022-04-16
+
+type EventPayload = {
+  id: string;
+  createdAt: string;
+  value: number;
+};
+
+export function normalisePayload(payload: EventPayload): EventPayload {
+  return {
+    id: payload.id.trim().toLowerCase(),
+    createdAt: new Date(payload.createdAt).toISOString(),
+    value: Number.isFinite(payload.value) ? payload.value : 0,
+  };
+}
