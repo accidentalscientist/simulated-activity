574d0c9def07357664e9024785c29ee466e50681
accidentalscientist <t.aymonier.newman@gmail.com>
2022-04-23 16:45:26 +1000

    aggregate csv metrics in go

---
+++ archive/go/2022/20220423_03_csv_aggregation_command.go
@@ -0,0 +1,12 @@
+// Archive task: Csv Aggregation Command
+// Generated for 2022-04-23
+
+package archive
+
+func SumByKey(rows []map[string]int, key string) int {
+	total := 0
+	for _, row := range rows {
+		total += row[key]
+	}
+	return total
+}
