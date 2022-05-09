2eeb77b6cd5ef541f915894ed37482016364a5f5
accidentalscientist <t.aymonier.newman@gmail.com>
2022-05-09 17:24:09 +1000

    define experiment metrics schema

---
+++ archive/sql/2022/20220509_04_schema_for_experiment_metrics.sql
@@ -0,0 +1,12 @@
+-- Archive task: Schema For Experiment Metrics
+-- Generated for 2022-05-09
+
+CREATE TABLE experiment_metrics (
+    experiment_id TEXT NOT NULL,
+    variant TEXT NOT NULL,
+    metric_date DATE NOT NULL,
+    visitors INTEGER NOT NULL,
+    conversions INTEGER NOT NULL,
+    revenue NUMERIC(12, 2),
+    PRIMARY KEY (experiment_id, variant, metric_date)
+);
