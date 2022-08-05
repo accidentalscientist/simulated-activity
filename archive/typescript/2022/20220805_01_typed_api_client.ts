b198e64ee929bb273411a22fdabcda4ade42ca17
accidentalscientist <t.aymonier.newman@gmail.com>
2022-08-05 13:01:28 +1000

    build typed api client

---
+++ archive/typescript/2022/20220805_01_typed_api_client.ts
@@ -0,0 +1,9 @@
+// Archive task: Typed Api Client
+// Generated for 2022-08-05
+
+type ApiResponse<T> = { data: T; status: number };
+
+export async function getJson<T>(url: string): Promise<ApiResponse<T>> {
+  const response = await fetch(url);
+  return { data: await response.json() as T, status: response.status };
+}
