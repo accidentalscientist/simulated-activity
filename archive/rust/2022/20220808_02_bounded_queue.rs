381d420ea023c555f94489215a4755bed538f353
accidentalscientist <t.aymonier.newman@gmail.com>
2022-08-08 15:11:52 +1000

    implement bounded queue in rust

---
+++ archive/rust/2022/20220808_02_bounded_queue.rs
@@ -0,0 +1,22 @@
+// Archive task: Bounded Queue
+// Generated for 2022-08-08
+
+use std::collections::VecDeque;
+
+pub struct BoundedQueue<T> {
+    limit: usize,
+    values: VecDeque<T>,
+}
+
+impl<T> BoundedQueue<T> {
+    pub fn new(limit: usize) -> Self {
+        Self { limit, values: VecDeque::new() }
+    }
+
+    pub fn push(&mut self, value: T) {
+        if self.values.len() == self.limit {
+            self.values.pop_front();
+        }
+        self.values.push_back(value);
+    }
+}
