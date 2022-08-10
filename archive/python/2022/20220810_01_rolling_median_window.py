182ada12dddbc261e793b347fddb283cba8a3447
accidentalscientist <t.aymonier.newman@gmail.com>
2022-08-10 11:32:04 +1000

    solve rolling median window in python

---
+++ archive/python/2022/20220810_01_rolling_median_window.py
@@ -0,0 +1,13 @@
+"""Archive task: Rolling Median Window.
+
+Generated for 2022-08-10. The implementation is intentionally compact so each
+commit can stand alone as a small learning artefact.
+"""
+
+from statistics import median
+
+
+def rolling_median(values: list[float], window: int) -> list[float]:
+    if window <= 0:
+        raise ValueError("window must be positive")
+    return [median(values[index:index + window]) for index in range(len(values) - window + 1)]
