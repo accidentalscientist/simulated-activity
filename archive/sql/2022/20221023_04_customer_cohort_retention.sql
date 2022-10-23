5153d2356337509240e127e242b3ed03dd96329b
accidentalscientist <t.aymonier.newman@gmail.com>
2022-10-23 19:44:29 +1100

    write customer cohort retention query

---
+++ archive/sql/2022/20221023_04_customer_cohort_retention.sql
@@ -0,0 +1,20 @@
+-- Archive task: Customer Cohort Retention
+-- Generated for 2022-10-23
+
+WITH first_seen AS (
+    SELECT customer_id, MIN(DATE_TRUNC('month', event_date)) AS cohort_month
+    FROM product_events
+    GROUP BY customer_id
+),
+activity AS (
+    SELECT customer_id, DATE_TRUNC('month', event_date) AS activity_month
+    FROM product_events
+    GROUP BY customer_id, DATE_TRUNC('month', event_date)
+)
+SELECT
+    first_seen.cohort_month,
+    activity.activity_month,
+    COUNT(*) AS retained_customers
+FROM first_seen
+JOIN activity USING (customer_id)
+GROUP BY first_seen.cohort_month, activity.activity_month;
