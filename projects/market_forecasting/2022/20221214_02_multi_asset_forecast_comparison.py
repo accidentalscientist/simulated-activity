4214b327b1c2ee243fdb2a41672bee1719518379
accidentalscientist <t.aymonier.newman@gmail.com>
2022-12-14 17:32:21 +1100

    compare forecast baselines

---
+++ projects/market_forecasting/2022/20221214_02_multi_asset_forecast_comparison.py
@@ -0,0 +1,12 @@
+"""Archive task: Multi-Asset Forecast Comparison.
+
+Generated for 2022-12-14. This synthetic market research code is educational
+only and is not financial advice.
+"""
+
+def compare_forecasts(actual, baselines: dict[str, list[float]]) -> dict[str, float]:
+    scores = {}
+    for name, prediction in baselines.items():
+        error = sum(abs(a - p) for a, p in zip(actual, prediction)) / len(actual)
+        scores[name] = error
+    return dict(sorted(scores.items(), key=lambda item: item[1]))
