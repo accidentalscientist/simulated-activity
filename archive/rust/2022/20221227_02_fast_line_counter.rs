8fdb07025717c20469da9c4cf1a7de8fc8fb0c8a
accidentalscientist <t.aymonier.newman@gmail.com>
2022-12-27 14:57:18 +1100

    add rust line counting utility

---
+++ archive/rust/2022/20221227_02_fast_line_counter.rs
@@ -0,0 +1,16 @@
+// Archive task: Fast Line Counter
+// Generated for 2022-12-27
+
+pub fn count_lines(input: &str) -> usize {
+    input.lines().count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn counts_lines() {
+        assert_eq!(count_lines("a\nb\nc"), 3);
+    }
+}
