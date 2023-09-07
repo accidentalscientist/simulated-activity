fc86972a6f78072607cd2214a961e7021b871fc6
accidentalscientist <t.aymonier.newman@gmail.com>
2023-09-07 11:34:13 +1000

    analyse origin energy volatility

---
+++ projects/market_forecasting/2023/20230907_01_origin_energy_volatility_study.py
@@ -0,0 +1,14 @@
+"""Archive task: Origin Energy Volatility Study.
+
+Generated for 2023-09-07. This synthetic market research code is educational
+only and is not financial advice.
+"""
+
+def rolling_volatility(values: list[float], window: int = 20) -> list[float]:
+    returns = [(values[i] / values[i - 1]) - 1 for i in range(1, len(values))]
+    output = []
+    for index in range(len(returns) - window + 1):
+        sample = returns[index:index + window]
+        mean = sum(sample) / window
+        output.append((sum((value - mean) ** 2 for value in sample) / window) ** 0.5)
+    return output
